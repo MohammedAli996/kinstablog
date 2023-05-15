56eb2155703ebea5af35a41f906b97c7dc269927
MOhammed Ali <2125937@talnet.nl>
2023-05-15 09:17:15 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+use App\Models\Article;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Index', [
+        'articles' => Article::latest()->get() 
+    ]);
+})->name('home');
+
+Route::get('/posts/{article:id}', function (Article $article) {
+    return Inertia::render('Show', [
+        'article' => $article
+    ]);
+})->name('article.show');
